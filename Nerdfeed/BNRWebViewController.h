3cfc056dc7d1a68538be9c9845c693e0d756caad
Kris Kata <kata.krzysztof@gmail.com>
2015-08-08 20:01:53 -0700

    Create web view for selected course

---
+++ Nerdfeed/BNRWebViewController.h
@@ -0,0 +1,15 @@
+//
+//  BNRWebViewController.h
+//  Nerdfeed
+//
+//  Created by Kris Kata on 8/8/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRWebViewController : UIViewController
+
+@property (nonatomic) NSURL *URL;
+
+@end
