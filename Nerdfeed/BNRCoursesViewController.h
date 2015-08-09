b527a61926df5b564e64167b69a36824c3cfab49
Kris Kata <kata.krzysztof@gmail.com>
2015-08-08 18:10:36 -0700

    Create table view controller

---
+++ Nerdfeed/BNRCoursesViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRCoursesViewController.h
+//  Nerdfeed
+//
+//  Created by Kris Kata on 8/8/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRCoursesViewController : UITableViewController
+
+@end
---
+++ Nerdfeed/BNRCoursesViewController.m
@@ -0,0 +1,23 @@
+//
+//  BNRCoursesViewController.m
+//  Nerdfeed
+//
+//  Created by Kris Kata on 8/8/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRCoursesViewController.h"
+
+@implementation BNRCoursesViewController
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+@end
