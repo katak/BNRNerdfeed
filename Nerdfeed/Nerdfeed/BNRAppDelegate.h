bd9596fb7f642b1bf4ee13b04f6e26a964b92907
Kris Kata <kata.krzysztof@gmail.com>
2015-08-08 17:32:25 -0700

    intial commit

---
+++ Nerdfeed/Nerdfeed/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Nerdfeed
+//
+//  Created by Kris Kata on 8/8/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
