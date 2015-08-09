13ed5a18e9d79f1442154193e92020a0cf27b96b
Kris Kata <kata.krzysztof@gmail.com>
2015-08-08 19:12:16 -0700

    Add method for retrieving json response from courses web service

--- Nerdfeed/BNRCoursesViewController.m
+++ Nerdfeed/BNRCoursesViewController.m
@@ -8,7 +8,28 @@
 
 #import "BNRCoursesViewController.h"
 
+@interface BNRCoursesViewController()
+
+@property (nonatomic, strong) NSURLSession *session;
+
+@end
+
 @implementation BNRCoursesViewController
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        self.navigationItem.title = @"BNR Courses";
+        
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config
+                                                 delegate:nil
+                                            delegateQueue:nil];
+        [self fetchFeed];
+    }
+    return self;
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
@@ -18,6 +39,20 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     return nil;
+}
+
+- (void)fetchFeed
+{
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask = [self.session  dataTaskWithRequest:req completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"%@", json);
+    }];
+    [dataTask resume];
 }
 
 @end
